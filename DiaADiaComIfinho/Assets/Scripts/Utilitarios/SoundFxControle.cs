6b48d7bb331159397a1ae4adb0c610e2b035ef61
joaosoliva <joaosoliva@gmail.com>
2018-05-04 16:00:42 -0300

    Lançamento

---
+++ DiaADiaComIfinho/Assets/Scripts/Utilitarios/SoundFxControle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFxControle : MonoBehaviour
+{
+   private AudioClip currentFx;
+   public void SetFx(AudioClip clip)
+    {
+        currentFx = clip;
+        GetComponent<AudioSource>().PlayOneShot(currentFx);
+    }
+}
